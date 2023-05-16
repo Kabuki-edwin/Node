c58c524df00d3cd11242497d928b9bc68aae784f
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-05-16 16:24:30 +0300

    stopped server

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 
+
+//routes
+
+app.get('/',(req, res) => {
+    res.send('Hello Node API')
+})
+
 app.listen(3000, () =>{
-    console.log('Node API app is rynning on port 3000')
+    console.log('Node API app is running on port 3000')
 })
