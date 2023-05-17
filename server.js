f5896e6e191b27b649910f28f5026b3009b77771
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-05-17 10:49:04 +0300

    added mongoose import

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 
 //routes
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello Node API')
 })
 
-app.get('/',(req, res) => {
-    res.send('Hello Node API')
+app.get('/blog', (req, res) => {
+    res.send('Hello Blog, my name is Edwin')
 }) 
 
 app.listen(3000, () => {
     console.log('Node API app is running on port 3000')
 })
+
+mongoose
+.connect('mongodb+srv://Admin:fez.co134205<password>@fezapi.mowli0w.mongodb.net/Node-API?retryWrites=true&w=majority')
