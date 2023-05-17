f3fcbd6024d41e69bac9b1b5d14412471d471bd2
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-05-17 10:05:50 +0300

    installed nodemon

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/',(req, res) => {
     res.send('Hello Node API')
 })
 
+app.get('/',(req, res) => {
+    res.send('Hello Node API')
+}) 
+
 app.listen(3000, () => {
     console.log('Node API app is running on port 3000')
 })
