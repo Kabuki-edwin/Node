aee41543fae3ef9d369021d6d2e4b019f68c4437
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-05-17 17:23:48 +0300

    fixing errors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const mongoose = require('mongoose')
+import express from "express";
+import mongoose from "mongoose";
+
 const app = express()
+const url = 'mongodb://localhost:27017/fezapi';
 
 
 //routes
@@ -13,14 +15,20 @@ app.get('/blog', (req, res) => {
     res.send('Hello Blog, my name is Edwin')
 }) 
 
-app.listen(3000, () => {
-    console.log('Node API app is running on port 3000')
-})
+/* mongodb+srv://Admin:fez.co134205<password>@fezapi.mowli0w.mongodb.net/Node-API?retryWrites=true&w=majority*/
 
 mongoose
-.connect('mongodb+srv://Admin:fez.co134205<password>@fezapi.mowli0w.mongodb.net/Node-API?retryWrites=true&w=majority')
+.connect(
+    url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+)
 .then(() => {
     console.log('connected to MongoDB')
+    app.listen(3000, () => {
+        console.log('Node API app is running on port 3000')
+    });
 }).catch((error) => {
-    console.log('error')
+    console.log('Error connecting to MongODB' , error)
 })
